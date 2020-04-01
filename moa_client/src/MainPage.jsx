1394954a2d05e06e48ceef7202ac07b3d959931e
EomJaeWoong <zzang6535@gmail.com>
2020-04-02 03:16:15 +0900

    기능: 방 생성, 방 목록 보여주기 기능 추가
    간단하게 방 생성, 방 목록 보여주기 기능을 만들었다
    방에 입장 전 생성, 목록을 보여주기 위해 mysql를 활용함

--- moa_client/src/MainPage.jsx
+++ moa_client/src/MainPage.jsx
@@ -8,45 +8,50 @@ import Posts from "./page/Posts";
 import UserInfo from "./page/UserInfo";
 import Room from "./page/Room";
 import SignupPage from "./page/SignupPage";
+import CreateRoomPage from "./page/CreateRoomPage";
 
 class MainPage extends Component {
   render() {
     let pageView;
 
     switch (this.props.pageValue) {
-      case "NotLogined":
-        pageView = (
-          <NotLoginedPage
-            Logined={this.props.Logined}
-            Signup={this.props.Signup}
-          />
-        );
-        break;
+        case "NotLogined":
+            pageView = (
+            <NotLoginedPage
+                Logined={this.props.Logined}
+                Signup={this.props.Signup}
+            />
+            );
+            break;
 
-      case "Logined":
-        pageView = <Posts CreateRoom={this.props.CreateRoom} />;
-        break;
+        case "Logined":
+            pageView = <Posts CreateRoom={this.props.CreateRoom} />;
+            break;
 
-      case "UserInfo":
-        pageView = <UserInfo />;
-        break;
+        case "UserInfo":
+            pageView = <UserInfo />;
+            break;
 
-      case "InRoom":
-        pageView = <Room />;
-        break;
+        case "InRoom":
+            pageView = <Room />;
+            break;
 
-      case "Signup":
-        pageView = <SignupPage />;
-        break;
+        case "Signup":
+            pageView = <SignupPage />;
+            break;
 
-      default:
-        // 잘못된 접근. 로그 남기고 무슨 값 들어왔는지 체크해야됨
-        // 404 페이지로 전환
-        pageView = <NotLoginedPage />;
-        break;
+        case "CreateRoom":
+            pageView = <CreateRoomPage Logined={this.props.Logined} />;
+            break;
+
+        default:
+            // 잘못된 접근. 로그 남기고 무슨 값 들어왔는지 체크해야됨
+            // 404 페이지로 전환
+            pageView = <NotLoginedPage />;
+            break;
     }
 
-    return <div>{pageView}</div>;
+    return <div className="mt-4">{pageView}</div>;
   }
 }
 
--- moa_client/src/page/CreateRoomPage.jsx
+++ moa_client/src/page/CreateRoomPage.jsx
@@ -1,26 +1,84 @@
 import React, { Component } from "react";
+import { Form, Button, Col, Row } from "react-bootstrap";
+import axios from "axios";
 
 class CreateRoomPage extends Component{
+    /////////////////////////////////////////////
+    // checkbox 상태 체크
+    /////////////////////////////////////////////
+    state = {
+        checked: false
+    };
+
+    handleChange = (e) => {
+        const { target: { checked } } = e;
+        this.setState({ checked });
+    };
+
+    /////////////////////////////////////////////
+    // 글 내용 보내기
+    /////////////////////////////////////////////
+    req_createRoom = async () => {
+        const send_param = this.state.checked? 
+        {
+            title: this._room_title.value,
+            url: this._room_url.value,
+            isSecret: this.state.checked,
+            password: this._room_pw.value,
+        } : 
+        {
+            title: this._room_title.value,
+            url: this._room_url.value,
+            isSecret: this.state.checked,
+        };
+
+        try{
+            const result = await axios.post("http://localhost:8080/room/create", send_param);
+            if(result.data.resultCode){
+                alert(result.data.msg);
+                this.props.Logined();
+            } else {
+                this._room_title.value = "";
+                this._room_url.value = "";
+                this._room_pw = "";
+                this.setState({ checked: false });
+            }
+        } catch(err){
+            console.log(err);
+        }
+    }
+
     render(){
+        const inputPwd = this.state.checked? 
+        <Form.Group>
+            <Form.Label>비밀번호</Form.Label>
+            <Form.Control type="password" ref={ref=>this._room_pw=ref} placeholder="비밀번호" />
+        </Form.Group> :
+        <div></div>;
+
         return(
-            <div class="container">
-                <form>
-                    <label>방 이름</label>
-                    <input type="text" style={inputStyle} placeholder="이름" />
-
-                    <label>URL</label>
-                    <input type="text" style={inputStyle} placeholder="URL" />
-
-                    <label>비밀방 여부</label>
-                    <input type="checkbox" style={inputStyle} value="secret_room" />
-
-                    <label>비밀번호</label>
-                    <input type="password" style={inputStyle} placeholder="비밀번호" />
-                   
-                    <button>만들기</button>
-                    <button>뒤로</button>
-                </form>
-            </div>
+            <Form style={{ width: "50%", margin: "0 auto" }}>
+                <Form.Group>
+                    <Form.Label>방 이름</Form.Label>
+                    <Form.Control type="text"  ref={ref=>this._room_title=ref} placeholder="이름" />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>URL</Form.Label>
+                    <Form.Control type="text"  ref={ref=>this._room_url=ref} placeholder="URL" />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Check type="checkbox" 
+                    checked={this.state.checked}
+                    onChange={this.handleChange} 
+                    label="비밀방 여부" />
+                </Form.Group>
+                {inputPwd}
+                
+                <Row>
+                    <Col><Button block onClick={this.req_createRoom} variant="primary">만들기</Button></Col>
+                    <Col><Button block onClick={this.props.Logined} variant="outline-default">뒤로</Button></Col>
+                </Row>
+            </Form>
         );
     }
 }
